6d84f530b228624837a6c337030f5a5782f32fd7
Jaider <jaidercueto.1010@gmail.com>
2023-02-21 13:30:18 -0500

    add sections

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import './App.css';
+import styles from'./App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Portfolio</h1>
+    <div className={styles.App}>
+      <nav className={styles.nav}>
+        <span>About me</span>
+        <span>Skills</span>
+        <span>Projects</span>
+        <span>Contact me</span>
+      </nav>
+      <main className={styles.main}>
+        <div className={styles.introduction}>
+            <div>
+                <h1>I am Jaider Cueto</h1>
+                <h3>I am a front-end developer</h3>
+            </div>
+        </div>
+        <div className={styles.skills}>
+
+        </div>
+        <div className={styles.myWork}>
+      
+        </div>
+        <div className={styles.contact}>
+
+        </div>
+      </main>
+      <footer className={styles.footer}>This is my footer</footer>
     </div>
   );
 }
