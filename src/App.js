49584a752c6794cfb1ab3ed73d7f324daa42e41a
Jaider <jaidercueto.1010@gmail.com>
2023-02-22 14:07:04 -0500

    about me, project section added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import styles from'./App.module.css';
 
 import img from './profile_picture-nbg.png'
+/* import novelty from './Novelty-books.png'
+import food from './Henry-food.png' */
 
 function App() {
   return (
@@ -22,12 +24,12 @@ function App() {
             <h2>About me</h2>
             <div>
               <img src={img} alt='profile'/>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting 
-                industry. Lorem Ipsum has been the industry's standard dummy text 
-                ever since the 1500s, when an unknown printer took a galley of type 
-                and scrambled it to make a type specimen book. It has survived not 
-                only five centuries, but also the leap into electronic typesetting, 
-                remaining essentially unchanged.</p>
+              <p>Hi, I'm Jaider. From the beginning I was interested 
+                in how computers work, so I decided to learn their 
+                language. I love to create attractive, fluid and 
+                efficient websites to offer solutions to problems and 
+                for personal taste. Here I share with you a little bit 
+                of what I like to do. Thanks for being here!</p>
             </div>
         </div>
         <div className={styles.skills}>
@@ -35,21 +37,27 @@ function App() {
             <div className={styles.skillsGrid}>
                 <div>
                   <h3>HTML & CSS</h3>
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt="html-logo" />
                 </div>
                 <div>
                   <h3>Javascript</h3>
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="js-logo"/>
                 </div>
                 <div>
                   <h3>React</h3>
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="react-logo"/>
                 </div>
                 <div>
                   <h3>Redux</h3>
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" alt="redux-logo"/>
                 </div>
                 <div>
                   <h3>Node</h3>
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" alt="node-logo"/>
                 </div>
                 <div>
                   <h3>Sequelize</h3>
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original.svg" alt="sql-logo"/>
                 </div>
             </div>
         </div>
@@ -58,9 +66,11 @@ function App() {
             <div className={styles.skillsWork}>
                 <div>
                   <h3>Henry Foods</h3>
+                  {/* <img src={food} alt="henry-food"/> */}
                 </div>
                 <div>
                   <h3>Novelty Books</h3>
+                 {/*  <img src={novelty} alt="novelty-books"/> */}
                 </div>
             </div>
         </div>
