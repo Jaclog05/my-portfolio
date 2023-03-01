ba2e317c1948e60d9dd734f93d20839e1663f2e1
Jaider <jaidercueto.1010@gmail.com>
2023-03-01 12:25:13 -0500

    cambios

--- src/App.js
+++ src/App.js
@@ -1,68 +1,70 @@
 import styles from'./App.module.css';
 
-import img from './profile_picture-nbg.png'
+import img from './Jaider_Cueto_Profile_picture.jpeg'
 
 function App() {
   return (
     <div className={styles.App}>
       <nav className={styles.nav}>
-        <span>About me</span>
-        <span>Skills</span>
-        <span>Projects</span>
-        <span>Contact me</span>
+        <a href='App.js#about'><span>About me</span></a>
+        <a href='App.js#my_skills'><span>Skills</span></a>
+        <a href='App.js#my_projects'><span>Projects</span></a>
+        <a href='App.js#contact_me'><span>Contact me</span></a>
       </nav>
       <main className={styles.main}>
         <div className={styles.introduction}>
             <div className={styles.typewriter}>
+                <img src={img} alt='profile'/>
                 <h1>Hi, I am Jaider Cueto.</h1>
                 <h3><span>I am a Full-Stack developer.</span></h3>
             </div>
         </div>
-        <div className={styles.about}>
+        <div id='about' className={styles.about}>
             <h2>About me</h2>
             <div>
-              <img src={img} alt='profile'/>
               <p>Hi, I'm Jaider. From the beginning I was interested 
                 in how computers work, so I decided to learn their 
                 language. I love to create attractive, fluid and 
                 efficient websites to offer solutions to problems and 
                 for personal taste. Here I share with you a little bit 
-                of what I like to do. Thanks for being here!</p>
+                of what I like to do. Thanks for being here!
+              </p>
+              <button>Download CV</button>
             </div>
         </div>
-        <div className={styles.skills}>
+        <div id='my_skills' className={styles.skills}>
             <h2>My Skills</h2>
             <div className={styles.skillsGrid}>
                 <div>
-                  <h3>HTML & CSS</h3>
+                  <h5>HTML & CSS</h5>
                   <div className={styles.htmlcss}>
                       <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt="html-logo" />
                       <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt="css-logo" />
                   </div>
                 </div>
                 <div>
-                  <h3>Javascript</h3>
+                  <h5>Javascript</h5>
                   <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="js-logo"/>
                 </div>
                 <div>
-                  <h3>React</h3>
+                  <h5>React</h5>
                   <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="react-logo"/>
                 </div>
                 <div>
-                  <h3>Redux</h3>
+                  <h5>Redux</h5>
                   <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" alt="redux-logo"/>
                 </div>
                 <div>
-                  <h3>Node</h3>
+                  <h5>Node</h5>
                   <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" alt="node-logo"/>
                 </div>
                 <div>
-                  <h3>Sequelize</h3>
+                  <h5>Sequelize</h5>
                   <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original.svg" alt="sql-logo"/>
                 </div>
             </div>
         </div>
-        <div className={styles.myWork}>
+        <div id='my_projects' className={styles.myWork}>
         <h2>My Projects</h2>
             <div className={styles.skillsWork}>
                 <a href='https://pi-foods-front-ten.vercel.app/'>
@@ -87,7 +89,7 @@ function App() {
                 </a>
             </div>
         </div>
-        <div className={styles.contact}>
+        <div id='contact_me' className={styles.contact}>
             <h2>Contact me!</h2>
             <div className={styles.skillsContact}>
                 <a href="https://www.linkedin.com/in/jaider-cueto-logreira"><i className="bi bi-linkedin"></i></a>
